944bb6f168d38140633a162f7bef487b9d773eb5
myeongjunkim <ilikerunning@naver.com>
2022-07-13 17:21:00 +0900

    fix:urls.py

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todomates/', include('todomates.urls')),
+    path('todomate/', include('todomates.urls')),
 ]
