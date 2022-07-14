fee9ee067c15c76dd127b5dd0c63eb8d13b1563a
myeongjunkim <ilikerunning@naver.com>
2022-07-14 16:56:05 +0900

    feat: admin costum

--- todomates/admin.py
+++ todomates/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+# admin.site.register(Category)
+admin.site.register(Todo)
+
+class Todo(admin.TabularInline):
+    model = Todo
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [
+        Todo,
+    ]
+
+admin.site.register(Category, CategoryAdmin)
+
